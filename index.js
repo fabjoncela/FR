1d114a3ecbc1a202d056596006548cbc05387ad5
Fabjon Cela <99296979+fabjoncela@users.noreply.github.com>
2024-08-02 13:37:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const imgElement = document.querySelector('.rick');
-    const videoElement = document.createElement('video');
-  
-    videoElement.setAttribute('width', '100%');
-    videoElement.setAttribute('controls', '');
-    videoElement.setAttribute('autoplay', '');
-    videoElement.innerHTML = `
-      <source src="./rickroll.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    `;
-  
-    imgElement.parentNode.replaceChild(videoElement, imgElement);
+  const imgElement = document.querySelector('.rick');
+  const videoElement = document.createElement('video');
+
+  videoElement.setAttribute('width', '100%');
+  videoElement.setAttribute('controls', '');
+  videoElement.setAttribute('autoplay', '');
+  videoElement.setAttribute('loop', '');
+  videoElement.setAttribute('muted', '');  // Mute the video to allow autoplay
+  videoElement.innerHTML = `
+    <source src="./rickroll.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+  `;
+
+  imgElement.parentNode.replaceChild(videoElement, imgElement);
+
+  // Programmatically unmute after starting
+  videoElement.addEventListener('canplay', () => {
+    videoElement.muted = false;
   });
-  
+});
